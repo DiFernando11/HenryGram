c36d9594e496e3f13e5794f226ede0787f25e781
DiFernando11 <102831050+DiFernando11@users.noreply.github.com>
2022-12-10 17:17:06 -0500

    route/protected/frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,44 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import { AuthProvider, AuthRoute, NotAuthRoute } from "./components/auth";
 import Home from "./components/Home";
+import Login from "./components/Login";
+import Logout from "./components/Logout";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <>
+    <AuthProvider>
       <NavBar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route
+          path="/home"
+          element={
+            <AuthRoute>
+              <Home />
+            </AuthRoute>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <NotAuthRoute>
+              <Login />
+            </NotAuthRoute>
+          }
+        />
+
+        <Route
+          path="/logout"
+          element={
+            <AuthRoute>
+              <Logout />
+            </AuthRoute>
+          }
+        />
         <Route path="*" element={<p>Not found</p>} />
       </Routes>
-    </>
+    </AuthProvider>
   );
 }
 
