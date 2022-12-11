d8b942acf567f162b9743457a757bf87e65d89ab
Nachoberridy <nachoberridy1997@gmail.com>
2022-12-10 22:52:38 -0300

    boilerplate back

---
+++ Api/src/controllers/postController.js
@@ -0,0 +1,78 @@
+const PostSchema = require('../models/Post');
+
+const postController = async (req, res) => {
+
+    /*
+        Controlador de la Ruta para realizar un posteo
+    */
+
+    const {
+        userId,
+        description,
+    } = req.body;
+
+    let image
+    let hidden
+    let isMatch
+
+    req.body.image? image = req.body.image : image = null;
+    req.body.hidden? hidden = req.body.hidden : hidden = false;
+    req.body.isMatch? isMatch = req.body.isMatch : isMatch = false;
+
+    const post = PostSchema({
+        userId,
+        description,
+        image,
+        hidden,
+        isMatch
+    });
+
+    post.save()
+        .then(post => {
+            res.status(200).json(post);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
+}
+
+const postCommentController = async (req, res) => {
+
+    /*
+        Controlador de la Ruta pora realizar un comentario
+    */
+
+    const {
+        postId,
+        userId,
+        description,
+    } = req.body;
+
+    const post = await PostSchema.findOne({ _id: postId
+    })
+
+
+    if (post) {
+        post.comments.push({
+            description: description,
+            userId: userId,
+            date: Date.now()
+        });
+        post.save()
+            .then(post => {
+                res.status(200).json(post);
+            })
+            .catch(err => {
+                res.status(500).json(err);
+            });
+    } else {
+        res.status(404).json({ message: "Post not found" });
+    }
+}
+
+        
+
+module.exports = {
+    postController,
+    postCommentController
+}
