3d8d89908d5034bcde186ceacbe41ca9f2fedc30
Luis <luismanlaz@hotmail..com>
2022-12-12 19:53:46 -0300

    feat/chat

---
+++ Api/src/routes/friend.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const {  } = require('../controllers/friendController');
+
+const router = express.Router();
+
+router.post('/friend', );
+
+module.exports = router;
