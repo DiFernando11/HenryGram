3e5be23efd3b7e0dd0312c0a334fcdbba7ef0f2e
Luis <luismanlaz@hotmail..com>
2022-12-12 17:11:46 -0300

    feat/chat

--- Api/src/server.js
+++ Api/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const userRoutes = require('./routes/user');
 const postRoutes = require('./routes/post');
+const messageRoutes = require('./routes/message');
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api", userRoutes);
 app.use("/api", postRoutes);
+app.use("/api", messageRoutes);
 
 // Global Variables
 
