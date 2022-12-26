a3bfd40db45bfdde2644b7eb97721b45a7c2f963
FacundoMartinez14 <facundo.eet2@gmail.com>
2022-12-26 16:19:34 -0300

    fixed AboutProfile

--- client/src/components/PageProfile/Profile/index.jsx
+++ client/src/components/PageProfile/Profile/index.jsx
@@ -1,34 +1,34 @@
-import React, {useEffect} from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { useParams } from "react-router-dom";
-import AboutProfile from "../AboutProfile";
-import HeaderProfile from "../HeaderProfile";
-import PostProfile from "../PostProfile";
-import { clearState } from "../../../redux/actions";
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import AboutProfile from '../AboutProfile';
+import HeaderProfile from '../HeaderProfile';
+import PostProfile from '../PostProfile';
 
 function Profile({ userInformation }) {
-  const dispatch = useDispatch()
-  const userInformationVerify = useSelector((state) => state.userInformation);
-  const userProfileFriend = useSelector((state) => state.userProfileFriend);
-  const { id } = useParams();
-  const handleIsFriend = () => {
-    let result;
-    if (userProfileFriend?._id === userInformationVerify?._id || !id)
-      result = false;
-    else result = true;
-    return result;
-  };
-  
-  const isFriend = handleIsFriend();
-  return (
-    <main className="w-full">
-      <HeaderProfile userInformation={userInformation} isFriend={isFriend} />
-      <div className=" w-full xl:flex sm:h-[calc(100vh-9rem)] lg:h-[calc(100vh-9rem)] h-[calc(100vh-13rem)] overflow-y-scroll" >
-        <AboutProfile userInformation={userInformation} isFriend={isFriend} />
-        <PostProfile userInformation={userInformation} isFriend={isFriend} />
-      </div>
-    </main>
-  );
+	const userInformationVerify = useSelector((state) => state.userInformation);
+	const userProfileFriend = useSelector((state) => state.userProfileFriend);
+	const [isFriend, setIsFriend] = useState(true);
+	const { id } = useParams();
+	
+	useEffect(() => {
+		setIsFriend(userProfileFriend?._id === userInformationVerify?._id);
+		return () => {
+			setIsFriend(false);
+		};
+	}, [id]);
+
+
+
+	return (
+		<main className="w-full">
+			<HeaderProfile userInformation={userInformation} isFriend={isFriend} />
+			<div className=" w-full xl:flex sm:h-[calc(100vh-9rem)] lg:h-[calc(100vh-9rem)] h-[calc(100vh-13rem)] overflow-y-scroll">
+				<AboutProfile userInformation={userInformation} isFriend={isFriend} />
+				<PostProfile userInformation={userInformation} isFriend={isFriend} />
+			</div>
+		</main>
+	);
 }
 
 export default Profile;
