f72497de639045e747e8e6e1b16d271e2a45d375
Luis <luismanlaz@hotmail..com>
2023-01-02 18:44:00 -0300

    feat/luis

--- Api/src/routes/group.js
+++ Api/src/routes/group.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { addChat, getAllChat, reqInvite, resInvite, getGroups } = require('../controllers/groupController')
+const { addChat, getAllChat, reqInvite, resInvite, getGroups, getRecommendedGroups } = require('../controllers/groupController')
 
 const router = express.Router();
 
@@ -7,7 +7,8 @@ router.post('/groups', addChat);
 router.get('/groups', getAllChat);
 router.post('/groups/req', reqInvite);
 router.post('/groups/res', resInvite);
-router.get('/groups/:userId', getGroups );
+router.get('/groups/:userId', getGroups);
+router.get('/groups/recommended/:userId', getRecommendedGroups);
 
 
 
